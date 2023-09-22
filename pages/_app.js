6f32c13cf025989f1f14da414ea0c1952546f153
tom <tomphill@live.co.uk>
2023-09-22 10:04:21 +0800

    add font awesome config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
